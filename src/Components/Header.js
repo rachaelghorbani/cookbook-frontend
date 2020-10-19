2dcb9c05e110d03cce2fe60fb97d0ef429843ffc
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-10-19 13:17:03 -0400

    playing around with header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Jumbotron, Container} from 'react-bootstrap'
+import styled from 'styled-components'
+import Food from '../assets/food.jpg'
 
+const Styles = styled.div`
+.jumbo {
+    background: src(${Food}) no-repeat fixed-bottom;
+    background-size: cover;
+    color: #efefef;
+    height: 200px;
+    position: relative;
+    z-index: -2;
+}
+`
 class Header extends React.Component {
+
+
+
     
 	render() {
 		return (
-			<div className="header">
-                <h1>HEADER GOES HERE!</h1>
-			</div>
+            <Styles>
+            <Jumbotron fluid className="jumbo">
+                <div className="overlay"></div>
+                <Container>
+                    <h1>Welcome</h1>
+                </Container>
+
+            </Jumbotron>
+            </Styles>
+			// <div className="header">
+            //     <h1>HEADER GOES HERE!</h1>
+			// </div>
 		);
 	}
 }
