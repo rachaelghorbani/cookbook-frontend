f454ea250044767dc2bf53704ade7a1e6dd0f5c0
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-10-19 20:18:14 -0400

    added recipe show cards

--- src/Components/CookbookShowPage.js
+++ src/Components/CookbookShowPage.js
@@ -1,15 +1,47 @@
-import React from 'react'
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container} from 'react-bootstrap'
+import { Container, Row, Col, CardGroup } from 'react-bootstrap';
+import RecipeCard from '../Components/RecipeCard';
 
-const CookbookShowPage = props => {
-    console.log(props)
-    return (
-        <Container>
-        <div>{props.cookbook.title}</div>
-        </Container>
-        
-    )
-}
+const CookbookShowPage = (props) => {
+	const renderRecipes = () => {
+		const recipes = props.cookbook.recipes.map((recipe) => <RecipeCard recipe={recipe} />);
+		return <CardGroup>{recipes}</CardGroup>;
+	};
+	const getTitleImage = () => {
+		let photoRecipe = props.cookbook.recipes.find((rec) => rec.photos.length > 0);
+		if (photoRecipe) {
+			return photoRecipe.photos[0].img_url;
+		} else {
+			return 'https://images2.minutemediacdn.com/image/upload/c_crop,h_1126,w_2000,x_0,y_181/f_auto,q_auto,w_1100/v1554932288/shape/mentalfloss/12531-istock-637790866.jpg';
+		}
+	};
+	console.log(props);
+	return (
+		<Container className="mt-2">
+			<Row>
+				<Col className="border border-primary">
+					<img className="img-responsive w-100" src={getTitleImage()} alt="food pic" />
+				</Col>
+				<Col className="border border-danger">
+					<Row>
+						<Col lg={12} className="border border-dark text-center h3">
+							{props.cookbook.title}
+						</Col>
+						<Col lg={12} className="border border-primary text-center bt-3">
+							{props.cookbook.description}
+						</Col>
+					</Row>
+				</Col>
+			</Row>
+			<Row className="mt-6">
+				<Col className="border border-info">
+                    {renderRecipes()}
+                </Col>
+			</Row>
+			{/* <div>{props.cookbook.title}</div> */}
+		</Container>
+	);
+};
 
-export default CookbookShowPage
+export default CookbookShowPage;
---
+++ src/Components/RecipeCard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+import {Link} from 'react-router-dom'
+
+const RecipeCard = (props) => {
+	const showPhoto = () => {
+		if (props.recipe.photos.length > 0) {
+			return props.recipe.photos[0].img_url;
+		}
+	};
+    console.log(props.recipe.photos)
+
+	return (
+		<Card style={{ width: '18rem', margin: '5px' }}>
+			<Card.Img variant="top" src={showPhoto()} alt="You Should See Food Here." />
+			<Card.Body>
+				<Card.Title>{props.recipe.title}</Card.Title>
+				<Card.Text>Cookbook description to go here!</Card.Text>
+				<Link to={`/recipes/${props.recipe.id}`}>
+					<Button variant="primary">See More</Button>
+				</Link>
+			</Card.Body>
+		</Card>
+	);
+};
+
+export default RecipeCard;
